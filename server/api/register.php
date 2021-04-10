d0ddb36096e4777a8aa40e7dc7a39ee57b7cde4a
mmvest <mvest96@gmail.com>
2021-04-10 14:11:02 -0500

    Update register.php

--- server/api/register.php
+++ server/api/register.php
@@ -5,27 +5,28 @@
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
-    include_once '../config/database.php';
-    include_once '../classes/user.php';
+    echo 'testing if this works';
+    // include_once '../config/database.php';
+    // include_once '../classes/user.php';
 
-    $database = new Database();
-    $db = $database->getConnection();
+    // $database = new Database();
+    // $db = $database->getConnection();
 
-    //Create the new user object with a connection to the database
-    $newUser = new User($db);
+    // //Create the new user object with a connection to the database
+    // $newUser = new User($db);
 
-    //Read in the data from the post
-    $data = json_decode(file_get_contents("php://input"));
+    // //Read in the data from the post
+    // $data = json_decode(file_get_contents("php://input"));
 
-    //Set variables
-    $newUser->username = $data->username;
-    $newUser->password = $data->password;
-    $newUser->created = date('Y-m-d H:i:s');
+    // //Set variables
+    // $newUser->username = $data->username;
+    // $newUser->password = $data->password;
+    // $newUser->created = date('Y-m-d H:i:s');
 
-    if($newUser->createUser())
-    {
-        echo 'Registered Successfully.';
-    } else {
-        echo 'Registration could not be completed.';
-    }
+    // if($newUser->createUser())
+    // {
+    //     echo 'Registered Successfully.';
+    // } else {
+    //     echo 'Registration could not be completed.';
+    // }
 ?>
