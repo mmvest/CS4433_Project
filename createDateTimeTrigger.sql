a0ecde8baa484d1b5624ca93ef957423ccd480ba
mmvest <mvest96@gmail.com>
2021-04-26 22:36:49 -0500

    Update createDateTimeTrigger.sql

--- createDateTimeTrigger.sql
+++ createDateTimeTrigger.sql
@@ -1,26 +1,29 @@
 USE test_timekeeper;
 
-DROP TRIGGER IF EXISTS datetime_check_insert;
+DROP TRIGGER IF EXISTS datetime_check_update;
 
 DELIMITER $$
-CREATE TRIGGER datetime_check_insert BEFORE INSERT ON entry
+CREATE TRIGGER datetime_check_update AFTER UPDATE ON entry
 FOR EACH ROW
 BEGIN
     DECLARE done INT DEFAULT FALSE;
+    DECLARE entryID BIGINT;
     DECLARE sdt, edt DATETIME;
-    DECLARE cur CURSOR FOR SELECT start_date_time, end_date_time FROM entry WHERE username = NEW.username;
+    DECLARE cur CURSOR FOR SELECT entry_id, start_date_time, end_date_time FROM entry WHERE username = NEW.username;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     OPEN cur;
 
     read_loop: LOOP
-        FETCH cur INTO sdt, edt;
+        FETCH cur INTO entryID, sdt, edt;
         IF done THEN
             LEAVE read_loop;
         END IF;
-        IF ((NEW.start_date_time < edt) AND (NEW.start_date_time > sdt)) OR ((NEW.end_date_time < edt) AND (NEW.end_date_time > sdt)) OR (NEW.start_date_time = sdt OR NEW.end_date_time = edt) THEN
-            SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'You can not insert entries that overlap with start or end times of other entries.';
+        IF (NEW.entry_id != entryID) THEN
+            IF ((NEW.start_date_time < edt) AND (NEW.start_date_time > sdt)) OR ((NEW.end_date_time < edt) AND (NEW.end_date_time > sdt)) OR (NEW.start_date_time = sdt OR NEW.end_date_time = edt) THEN
+                SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'You can not insert entries that overlap with start or end times of other entries.';
+            END IF;
         END IF;
     END LOOP;
 
